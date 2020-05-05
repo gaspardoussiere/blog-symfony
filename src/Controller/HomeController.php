dff163937d61d82ddc6b9b4d4430854cbeb6c0bc
gaspardoussiere <gaspeuh@hotmail.fr>
2020-05-05 11:17:39 +0200

    Installation de base + annotations

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+
+        return new Response("Hello World!");
+    }
+}
